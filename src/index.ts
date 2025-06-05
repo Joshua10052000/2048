3bffaaf9e3bb518658dfe05577495df09416ba3e
Jan Joshua De Guzman <wawaguzman10052000@gmail.com>
2025-06-05 19:45:08 +0800

    Converting style to tailwindcss

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
-import Grid from "@/model/grid";
+import "@/styles/globals";
 
-const nodeGrid = document.querySelector("article");
-if (!nodeGrid) throw new Error("nodeBoard is undefined.");
-
-const grid = new Grid({ nodeGrid });
-
-const rightKeys = ["ArrowRight", "D", "d"];
-const leftKeys = ["ArrowLeft", "A", "a"];
-const topKeys = ["ArrowUp", "W", "w"];
-const bottomKeys = ["ArrowDown", "S", "s"];
-
-grid.generateTile();
-grid.generateTile();
-
-document.addEventListener("keydown", (e) => {
-  if (["ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown"].includes(e.key)) {
-    e.preventDefault();
-  }
-
-  if (rightKeys.includes(e.key)) {
-    grid.moveRight();
-  } else if (leftKeys.includes(e.key)) {
-    grid.moveLeft();
-  } else if (topKeys.includes(e.key)) {
-    grid.moveTop();
-  } else if (bottomKeys.includes(e.key)) {
-    grid.moveBottom();
-  }
-});
+const rootNode = document.querySelector<HTMLDivElement>("#root")!;
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const config = {
     ],
   },
   resolve: {
-    extensions: [".js", ".ts"],
+    extensions: [".js", ".ts", ".css"],
     alias: {
       "@": path.resolve("./src/"),
     },
